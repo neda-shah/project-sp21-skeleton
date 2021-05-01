2f7b8689fedbe44d456dd10b562731de2e8cbdf5
Neda Shahiar <nedashahiar@berkeley.edu>
2021-05-01 16:50:27 -0700

    updated solver.py

--- solver.py
+++ solver.py
@@ -5,6 +5,16 @@ import sys
 from os.path import basename, normpath
 import glob
 
+# takes in a graph G and returns the number of cities that can be removed and 
+# the number of edges that can be removed (k, c)
+def find_params(G):
+	n = nx.number_of_nodes(G)
+	if 20 <= n <= 30:
+		return (15, 1)
+	if 31 <= n <= 50:
+		return (50, 3)
+	if 51 <= n <= 100:
+		return (100, 5)
 
 def solve(G):
     """
@@ -14,21 +24,64 @@ def solve(G):
         c: list of cities to remove
         k: list of edges to remove
     """
-    pass
+    c = [] # list of cities to remove
+    k = [] # list of edges to remove
+    n = nx.number_of_nodes(G)
+    e, v = find_params(G) 
+    H = G.copy()
+    # Handles removing nodes
+    while v:
+    	max_city = None
+    	max_score = 0
+    	shortest = nx.single_source_dijkstra_path(H, 0)[n - 1] # get the shortest path from s to t
+    	for i in range(1, len(shortest) - 1): # iterate through nodes in the shortest path
+    		curr = shortest[i] # current node
+    		if is_valid_solution(H, [curr], []): # check that graph won't 
+    			temp_score = calculate_score(H, [curr], [])
+    			if temp_score > max_score:
+    				max_score = temp_score
+    				max_city = curr
+    	if max_city is None:
+    		break
+    	c.append(max_city)
+    	H.remove_nodes_from([max_city])
+    	v -= 1
+    
+    # Handles removing edges
+    while e:
+    	max_edge = None
+    	max_score = 0
+    	shortest = nx.single_source_dijkstra_path(H, 0)[n - 1]
+    	for i in range(0, len(shortest) - 1):
+    		curr = (shortest[i], shortest[i + 1])
+    		if is_valid_solution(H, [], [curr]):
+    			temp_score = calculate_score(H, [], [curr])
+    			if temp_score > max_score:
+    				max_score = temp_score
+    				max_edge = curr
+    	if max_edge is None:
+    		break
+    	k.append(max_edge)
+    	H.remove_edges_from([max_edge])
+    	e -= 1
+    return c, k
+
+
+
 
 
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
 
-# if __name__ == '__main__':
-#     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G = read_input_file(path)
-#     c, k = solve(G)
-#     assert is_valid_solution(G, c, k)
-#     print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
-#     write_output_file(G, c, k, 'outputs/small-1.out')
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G = read_input_file(path)
+    c, k = solve(G)
+    assert is_valid_solution(G, c, k)
+    print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
+    write_output_file(G, c, k, 'outputs/small-1.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
